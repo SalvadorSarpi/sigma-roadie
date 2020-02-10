b15117e6b4f4719c866855af3dba312d9fb32d53
Salvador Sarpi <ssarpi@outlier.cl>
2020-02-10 20:46:29 -0300

    hub

--- src/Sigma.Roadie.Server/Orquestrator/OrquestratorHub.cs
+++ src/Sigma.Roadie.Server/Orquestrator/OrquestratorHub.cs
@@ -9,19 +9,23 @@ namespace Sigma.Roadie.Server.Orquestrator
 
     public class OrquestratorHub : Hub
     {
+
         public override Task OnConnectedAsync()
         {
-            Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} joined the conversation");
+            //Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} joined the conversation");
             return base.OnConnectedAsync();
         }
+
+
         public void Send(string name, string message)
         {
             Clients.All.SendAsync("broadcastMessage", name, message);
         }
 
+
         public override Task OnDisconnectedAsync(System.Exception exception)
         {
-            Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} left the conversation");
+            //Clients.All.SendAsync("broadcastMessage", "system", $"{Context.ConnectionId} left the conversation");
             return base.OnDisconnectedAsync(exception);
         }
     }
