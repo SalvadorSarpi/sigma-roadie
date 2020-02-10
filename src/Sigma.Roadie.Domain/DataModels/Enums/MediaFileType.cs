fae5bca88faff6aceda98b4207d13a6805aaf636
Salvador Sarpi <ssarpi@outlier.cl>
2020-02-10 20:25:01 -0300

    media file type

---
+++ src/Sigma.Roadie.Domain/DataModels/Enums/MediaFileType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sigma.Roadie.Domain.DataModels.Enums
+{
+    public enum MediaFileType
+    {
+
+        Audio = 1,
+        Video = 2
+
+    }
+
+}
---
+++ src/Sigma.Roadie.Domain/DataModels/Partials/MediaFile.partial.cs
@@ -0,0 +1,18 @@
+﻿using Sigma.Roadie.Domain.DataModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sigma.Roadie.Domain.DataModels
+{
+    public partial class MediaFile
+    {
+
+        public MediaFileType TypeEnum
+        {
+            get => (MediaFileType)this.Type;
+            set => this.Type = (short)value;
+        }
+
+    }
+}
