8d9608af98cef795095b273b594d9d8b812fbac1
Salvador Sarpi <ssarpi@outlier.cl>
2020-02-10 18:35:15 -0300

    removing sample files

--- src/Sigma.Roadie.Server/Startup.cs
+++ src/Sigma.Roadie.Server/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sigma.Roadie.Server.Data;
 
 namespace Sigma.Roadie.Server
 {
@@ -27,7 +26,6 @@ namespace Sigma.Roadie.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
