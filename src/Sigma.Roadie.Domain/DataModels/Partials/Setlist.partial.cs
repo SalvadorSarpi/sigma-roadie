9abd57736dfd8877191f09702bedd96d61318b32
Salvador Sarpi <ssarpi@outlier.cl>
2020-02-11 00:40:09 -0300

    setlist lenght

---
+++ src/Sigma.Roadie.Domain/DataModels/Partials/Setlist.partial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Linq;
+
+namespace Sigma.Roadie.Domain.DataModels
+{
+
+    public partial class Setlist
+    {
+
+        [NotMapped]
+        public TimeSpan TotalLenght => new TimeSpan(SetlistScene.Select(q => q.Scene.Duration.Ticks).Sum());
+
+    }
+
+}
